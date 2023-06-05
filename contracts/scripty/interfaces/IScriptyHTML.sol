1b7822fe421e6b0c64c0038d1225309beebbf345
0xdude <thedudecrypto@gmail.com>
2023-06-05 23:50:28 +0100

    test on an example

--- contracts/scripty/interfaces/IScriptyHTML.sol
+++ contracts/scripty/interfaces/IScriptyHTML.sol
@@ -13,67 +13,67 @@ pragma solidity ^0.8.17;
 import {HTMLRequest, HTMLTagType, HTMLTag} from "./../core/ScriptyCore.sol";
 
 interface IScriptyHTML {
-    // // =============================================================
-    // //                      RAW HTML GETTERS
-    // // =============================================================
+    // =============================================================
+    //                      RAW HTML GETTERS
+    // =============================================================
 
-    // /**
-    //  * @notice  Get HTML with requested head tags and scripts housed in
-    //  *          multiple <script> tags
-    //  * @dev @dev Your HTML is returned in the following format:
-    //  *      <html>
-    //  *          <head>
-    //  *              [tagOpen[0]][tagContent[0]][tagClose[0]]
-    //  *              [tagOpen[1]][tagContent[1]][tagClose[1]]
-    //  *              ...
-    //  *              [tagOpen[n]][tagContent[n]][tagClose[n]]
-    //  *          </head>
-    //  *          <body>
-    //  *              [tagOpen[0]]{request[0]}[tagClose[0]]
-    //  *              [tagOpen[1]]{request[1]}[tagClose[1]]
-    //  *              ...
-    //  *              [tagOpen[n]]{request[n]}[tagClose[n]]
-    //  *          </body>
-    //  *      </html>
-    //  * @param htmlRequest - HTMLRequest
-    //  * @return Full HTML with head and script tags
-    //  */
-    // function getHTML(
-    //     HTMLRequest memory htmlRequest
-    // ) external view returns (bytes memory);
+    /**
+     * @notice  Get HTML with requested head tags and scripts housed in
+     *          multiple <script> tags
+     * @dev @dev Your HTML is returned in the following format:
+     *      <html>
+     *          <head>
+     *              [tagOpen[0]][tagContent[0]][tagClose[0]]
+     *              [tagOpen[1]][tagContent[1]][tagClose[1]]
+     *              ...
+     *              [tagOpen[n]][tagContent[n]][tagClose[n]]
+     *          </head>
+     *          <body>
+     *              [tagOpen[0]]{request[0]}[tagClose[0]]
+     *              [tagOpen[1]]{request[1]}[tagClose[1]]
+     *              ...
+     *              [tagOpen[n]]{request[n]}[tagClose[n]]
+     *          </body>
+     *      </html>
+     * @param htmlRequest - HTMLRequest
+     * @return Full HTML with head and script tags
+     */
+    function getHTML(
+        HTMLRequest memory htmlRequest
+    ) external view returns (bytes memory);
 
-    // // =============================================================
-    // //                      ENCODED HTML GETTERS
-    // // =============================================================
+    // =============================================================
+    //                      ENCODED HTML GETTERS
+    // =============================================================
 
-    // /**
-    //  * @notice Get {getHTML} and base64 encode it
-    //  * @param htmlRequest - HTMLRequest
-    //  * @return Full HTML with head and script tags, base64 encoded
-    //  */
-    // function getEncodedHTML(
-    //     HTMLRequest memory htmlRequest
-    // ) external view returns (bytes memory);
+    /**
+     * @notice Get {getHTML} and base64 encode it
+     * @param htmlRequest - HTMLRequest
+     * @return Full HTML with head and script tags, base64 encoded
+     */
+    function getEncodedHTML(
+        HTMLRequest memory htmlRequest
+    ) external view returns (bytes memory);
 
-    // // =============================================================
-    // //                      STRING UTILITIES
-    // // =============================================================
+    // =============================================================
+    //                      STRING UTILITIES
+    // =============================================================
 
-    // /**
-    //  * @notice Convert {getHTML} output to a string
-    //  * @param htmlRequest - HTMLRequest
-    //  * @return {getHTMLWrapped} as a string
-    //  */
-    // function getHTMLString(
-    //     HTMLRequest memory htmlRequest
-    // ) external view returns (string memory);
+    /**
+     * @notice Convert {getHTML} output to a string
+     * @param htmlRequest - HTMLRequest
+     * @return {getHTMLWrapped} as a string
+     */
+    function getHTMLString(
+        HTMLRequest memory htmlRequest
+    ) external view returns (string memory);
 
-    // /**
-    //  * @notice Convert {getEncodedHTML} output to a string
-    //  * @param htmlRequest - HTMLRequest
-    //  * @return {getEncodedHTML} as a string
-    //  */
-    // function getEncodedHTMLString(
-    //     HTMLRequest memory htmlRequest
-    // ) external view returns (string memory);
+    /**
+     * @notice Convert {getEncodedHTML} output to a string
+     * @param htmlRequest - HTMLRequest
+     * @return {getEncodedHTML} as a string
+     */
+    function getEncodedHTMLString(
+        HTMLRequest memory htmlRequest
+    ) external view returns (string memory);
 }
--- contracts/scripty/interfaces/IScriptyHTMLURLSafe.sol
+++ contracts/scripty/interfaces/IScriptyHTMLURLSafe.sol
@@ -13,52 +13,52 @@ pragma solidity ^0.8.17;
 import {HTMLRequest, HTMLTagType, HTMLTag} from "./../core/ScriptyCore.sol";
 
 interface IScriptyHTMLURLSafe {
-    // // =============================================================
-    // //                      RAW HTML GETTERS
-    // // =============================================================
+    // =============================================================
+    //                      RAW HTML GETTERS
+    // =============================================================
 
-    // /**
-    //  * @notice  Get URL safe HTML with requested head tags and scripts housed
-    //  *          in single <script> tag
-    //  * @dev Any tag type 0 scripts are converted to base64 and wrapped
-    //  *      with <script src="data:text/javascript;base64,[SCRIPT]"></script>
-    //  *
-    //  *      [WARNING]: Large non-base64 libraries that need base64 encoding
-    //  *      carry a high risk of causing a gas out. Highly advised the use
-    //  *      of base64 encoded scripts where possible
-    //  *
-    //  *      Your HTML is returned in the following format:
-    //  *      <html>
-    //  *          <head>
-    //  *              [tagOpen[0]][tagContent[0]][tagClose[0]]
-    //  *              [tagOpen[1]][tagContent[1]][tagClose[1]]
-    //  *              ...
-    //  *              [tagOpen[n]][tagContent[n]][tagClose[n]]
-    //  *          </head>
-    //  *          <body>
-    //  *              [tagOpen[0]]{request[0]}[tagClose[0]]
-    //  *              [tagOpen[1]]{request[1]}[tagClose[1]]
-    //  *              ...
-    //  *              [tagOpen[n]]{request[n]}[tagClose[n]]
-    //  *          </body>
-    //  *      </html>
-    //  * @param htmlRequest - HTMLRequest
-    //  * @return Full URL safe HTML with head and script tags
-    //  */
-    // function getHTMLURLSafe(
-    //     HTMLRequest memory htmlRequest
-    // ) external view returns (bytes memory);
+    /**
+     * @notice  Get URL safe HTML with requested head tags and scripts housed
+     *          in single <script> tag
+     * @dev Any tag type 0 scripts are converted to base64 and wrapped
+     *      with <script src="data:text/javascript;base64,[SCRIPT]"></script>
+     *
+     *      [WARNING]: Large non-base64 libraries that need base64 encoding
+     *      carry a high risk of causing a gas out. Highly advised the use
+     *      of base64 encoded scripts where possible
+     *
+     *      Your HTML is returned in the following format:
+     *      <html>
+     *          <head>
+     *              [tagOpen[0]][tagContent[0]][tagClose[0]]
+     *              [tagOpen[1]][tagContent[1]][tagClose[1]]
+     *              ...
+     *              [tagOpen[n]][tagContent[n]][tagClose[n]]
+     *          </head>
+     *          <body>
+     *              [tagOpen[0]]{request[0]}[tagClose[0]]
+     *              [tagOpen[1]]{request[1]}[tagClose[1]]
+     *              ...
+     *              [tagOpen[n]]{request[n]}[tagClose[n]]
+     *          </body>
+     *      </html>
+     * @param htmlRequest - HTMLRequest
+     * @return Full URL safe HTML with head and script tags
+     */
+    function getHTMLURLSafe(
+        HTMLRequest memory htmlRequest
+    ) external view returns (bytes memory);
 
-    // // =============================================================
-    // //                      STRING UTILITIES
-    // // =============================================================
+    // =============================================================
+    //                      STRING UTILITIES
+    // =============================================================
 
-    // /**
-    //  * @notice Convert {getHTMLURLSafe} output to a string
-    //  * @param htmlRequest - HTMLRequest
-    //  * @return {getHTMLURLSafe} as a string
-    //  */
-    // function getHTMLURLSafeString(
-    //     HTMLRequest memory htmlRequest
-    // ) external view returns (string memory);
+    /**
+     * @notice Convert {getHTMLURLSafe} output to a string
+     * @param htmlRequest - HTMLRequest
+     * @return {getHTMLURLSafe} as a string
+     */
+    function getHTMLURLSafeString(
+        HTMLRequest memory htmlRequest
+    ) external view returns (string memory);
 }
